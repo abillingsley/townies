f34b6bf0f6aa7b3951837fb5a0a10641313d2877
Preston Sego <LPSego3+dev@gmail.com>
2018-02-09 12:10:03 -0500

    re-organize some things

--- packages/web/src/ui/index.tsx
+++ packages/web/src/ui/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {AppContainer} from "react-hot-loader";
 
-import App from "./containers/App";
+import App from "./components/App";
 
 const rootDOMElement = document.getElementById("app");
 
@@ -18,8 +18,8 @@ const render = (AppComponent: any) => {
 render(App);
 
 if ((module as any).hot) {
-  (module as any).hot.accept("./containers/App", () => {
-    const NextApp = require("./containers/App").default; // tslint:disable-line no-var-requires
+  (module as any).hot.accept("./components/App", () => {
+    const NextApp = require("./components/App").default; // tslint:disable-line no-var-requires
     render(NextApp);
   });
 }
